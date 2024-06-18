a3339c353262494d48df14f677ed2a12002c94ac
hjlim7831 <hjlim7831@gmail.com>
2024-06-18 21:23:34 +0900

    ch12 : 12절 json 주석 추가

--- ch12/cmd/housework.go
+++ ch12/cmd/housework.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/awoodbeck/gnp/ch12/housework"
+	// 직렬화 포맷 임포트
 	storage "github.com/awoodbeck/gnp/ch12/json"
 	// storage "github.com/awoodbeck/gnp/ch12/gob"
 	// storage "github.com/awoodbeck/gnp/ch12/protobuf"
@@ -22,6 +23,7 @@ func init() {
 
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(),
+			// 커맨드 라인의 매개변수와 사용법을 나타냄
 			`Usage: %s [flags] [add chore, ...|complete #]
     add         add comma-separated chores
     complete    complete designated chore
@@ -32,11 +34,14 @@ Flags:
 	}
 }
 
+// housework.Chore 구조체 인스턴스의 포인터 슬라이스를 반환
 func load() ([]*housework.Chore, error) {
+	// 데이터 파일이 존재하지 않을 경우, 공백 슬라이스를 반환하며 종료됨
+	// 이는 애플리케이션을 최초로 실행했을 때 발생
 	if _, err := os.Stat(dataFile); os.IsNotExist(err) {
 		return make([]*housework.Chore, 0), nil
 	}
-
+	// 애플리케이션이 데이터 파일을 읽은 경우, 해당 파일을 열고
 	df, err := os.Open(dataFile)
 	if err != nil {
 		return nil, err
@@ -46,11 +51,12 @@ func load() ([]*housework.Chore, error) {
 			fmt.Printf("closing data file: %v", err)
 		}
 	}()
-
+	// io.Reader를 매개변수로 받는 스토리지의 Load 함수로 전달
 	return storage.Load(df)
 }
 
 func flush(chores []*housework.Chore) error {
+	// 새로운 파일을 만들거나 이미 존재하는 파일을 버리고
 	df, err := os.Create(dataFile)
 	if err != nil {
 		return err
@@ -61,20 +67,23 @@ func flush(chores []*housework.Chore) error {
 		}
 	}()
 
+	// 해당 파일 포인터와 함께 집안일 슬라이스를 스토리지의 Flush 함수에 전달
 	return storage.Flush(df, chores)
 }
 
 func list() error {
+	// 스토리지로부터 집안일 목록을 로드
 	chores, err := load()
 	if err != nil {
 		return err
 	}
 
+	// 로드된 목록에 아무런 집안일이 없다면, 없는대로 표준 출력에 출력
 	if len(chores) == 0 {
 		fmt.Println("You're all caught up!")
 		return nil
 	}
-
+	// 집안일이 있으면, 아래처럼 헤더와 내용을 출력
 	fmt.Println("#\t[X]\tDescription")
 	for i, chore := range chores {
 		c := " "
@@ -88,11 +97,13 @@ func list() error {
 }
 
 func add(s string) error {
+	// 1) 집안일 목록을 스토리지에서 가져온 후
 	chores, err := load()
 	if err != nil {
 		return err
 	}
-
+	// 2) 수정한 뒤
+	// 동시에 하나 이상의 집안일을 목록에 추가하기 위해, 여러 집안일의 설명을 쉼표로 구분된 문자열로 받음
 	for _, chore := range strings.Split(s, ",") {
 		if desc := strings.TrimSpace(chore); desc != "" {
 			chores = append(chores, &housework.Chore{
@@ -100,10 +111,11 @@ func add(s string) error {
 			})
 		}
 	}
-
+	// 3) 변화된 부분을 다시 스토리지에 저장
 	return flush(chores)
 }
 
+// 매개변수로 완료하고자 하는 집안일을 커맨드 라인에서 받은 후, 정수로 변환함
 func complete(s string) error {
 	i, err := strconv.Atoi(s)
 	if err != nil {
@@ -118,9 +130,10 @@ func complete(s string) error {
 	if i < 1 || i > len(chores) {
 		return fmt.Errorf("chore %d not found", i)
 	}
-
+	// 집안일의 목록 번호로 0이 아닌 1부터 시작함
+	// 슬라이스에 맞춰, 1을 빼주어야 함
 	chores[i-1].Complete = true
-
+	// 변경된 부분을 스토리지에 저장
 	return flush(chores)
 }
 
--- ch12/json/housework.go
+++ ch12/json/housework.go
@@ -10,9 +10,15 @@ import (
 func Load(r io.Reader) ([]*housework.Chore, error) {
 	var chores []*housework.Chore
 
+	// json.NewDecoder 함수에 매개변수로 io.Reader 인터페이스를 받은 뒤, 디코더를 반환
+	// 집안일 슬라이스의 포인터를 매개변수로 전달해, 디코더의 Decode 메서드를 호출
+	// 디코더는 io.Reader로부터 JSON 데이터를 읽어서 역직렬화한 뒤, 집안일 슬라이스를 만들어 냄
 	return chores, json.NewDecoder(r).Decode(&chores)
 }
 
+// io.Writer와 집안일 슬라이스를 매개변수로 받음
 func Flush(w io.Writer, chores []*housework.Chore) error {
+	// json.NewEncoder 함수에 매개변수로 io.Writer를 전달
+	// JSON 데이터를 직렬화한 뒤 해당 데이터를 io.Writer로 씀
 	return json.NewEncoder(w).Encode(chores)
 }
